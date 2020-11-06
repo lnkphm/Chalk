ec064f41d53a1dd71b81b4ccbe6843ee5b60b9ff
Linh Pham <lnkphm@gmail.com>
2020-11-06 15:42:51 +0700

    Update home page

--- client/src/components/CourseList.jsx
+++ client/src/components/CourseList.jsx
@@ -1,16 +1,111 @@
 import React from 'react';
+import axios from 'axios';
+import { Link as RouteLink } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Divider from '@material-ui/core/Divider';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import Link from '@material-ui/core/Link';
+
+import MoreVertIcon from '@material-ui/icons/MoreVert';
 
 const useStyles = makeStyles((theme) => ({
-  root: {},
+  root: {
+    marginTop: theme.spacing(2),
+  },
+  buttonSearch: {
+    marginLeft: 'auto',
+  },
 }));
 
 export default function CourseList(props) {
   const classes = useStyles();
+  const [courses, setCourses] = React.useState([]);
+  const [filter, setFilter] = React.useState({
+    category: '',
+    query: '',
+  });
+
+  React.useEffect(() => {
+    axios
+      .get(`/api/courses`)
+      .then((res) => {
+        setCourses(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
   return (
     <Container className={classes.root} maxWidth="md">
-      <h1>Course List</h1>
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={12}>
+          <Card>
+            <CardHeader title="Courses" />
+            <CardActions>
+              <Button variant="outlined" color="primary">
+                Create new course
+              </Button>
+            </CardActions>
+          </Card>
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          <Card>
+            <CardHeader title="Search" />
+            <Divider />
+            <CardContent>
+              <Grid container spacing={1}>
+                <Grid item xs={12}>
+                  <TextField label="Search" variant="outlined" fullWidth />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField label="Category" variant="outlined" fullWidth />
+                </Grid>
+              </Grid>
+            </CardContent>
+            <Divider />
+            <CardActions>
+              <Button
+                className={classes.buttonSearch}
+                color="primary"
+                variant="outlined"
+              >
+                Search
+              </Button>
+            </CardActions>
+          </Card>
+        </Grid>
+        <Grid item xs={12} sm={8}>
+          <Grid container spacing={2}>
+            {courses.map((item, index) => (
+              <Grid key={index} item xs={12}>
+                <Card>
+                  <CardHeader
+                    title={
+                      <Link component={RouteLink} to={`/courses/${item._id}`}>
+                        {item.name}
+                      </Link>
+                    }
+                    action={
+                      <IconButton>
+                        <MoreVertIcon />
+                      </IconButton>
+                    }
+                  />
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        </Grid>
+      </Grid>
     </Container>
   );
 }
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,15 +2,20 @@ import React from 'react';
 import { Link as RouteLink } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
-import Paper from '@material-ui/core/Paper';
 import Container from '@material-ui/core/Container';
 import Link from '@material-ui/core/Link';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import CardHeader from '@material-ui/core/CardHeader';
+import Divider from '@material-ui/core/Divider';
 import UserContext from '../contexts/UserContext';
+import Typography from '@material-ui/core/Typography';
+
+import Calendar from 'react-calendar';
 
 const useStyles = makeStyles((theme) => ({
   container: {
     paddingTop: theme.spacing(2),
-    paddingBottom: theme.spacing(4),
   },
   paper: {
     padding: theme.spacing(5),
@@ -21,31 +26,58 @@ const useStyles = makeStyles((theme) => ({
 export default function Home() {
   const data = React.useContext(UserContext);
   const courses = data.user.courses;
+  const [date, setDate] = React.useState(new Date());
   const classes = useStyles();
   return (
     <Container maxWidth="md" className={classes.container}>
       <Grid container spacing={2}>
-        <Grid item xs={3}>
+        <Grid item xs={12} sm={4}>
           <Grid container spacing={2}>
             <Grid item xs={12}>
-              <Paper className={classes.paper}>Upcoming</Paper>
+              <Card>
+                <CardHeader title="Upcoming" />
+                <Divider />
+                <CardContent>
+                  <Typography>There is no upcoming</Typography>
+                </CardContent>
+              </Card>
             </Grid>
             <Grid item xs={12}>
-              <Paper className={classes.paper}>Calendar</Paper>
+              <Card>
+                <CardHeader title="Calendar" />
+                <Divider />
+                <CardContent>
+                  <Calendar value={date} onChange={setDate} />
+                </CardContent>
+              </Card>
             </Grid>
           </Grid>
         </Grid>
-        <Grid item xs={9}>
+        <Grid item xs={12} sm={8}>
           <Grid container spacing={2}>
-            {courses.map((item, index) => (
+            <Grid item xs={12}>
+              <Typography variant="h4">Courses</Typography>
+              <Divider />
+            </Grid>
+            {(courses.length > 0) ? courses.map((item, index) => (
               <Grid key={index} item xs={12}>
-                <Paper className={classes.paper}>
-                  <Link component={RouteLink} to={`/courses/${item._id}`}>
-                    {item.name}
-                  </Link>
-                </Paper>
+                <Card>
+                  <CardHeader title={<Link component={RouteLink} to={`/courses/${item._id}`}>{item.name}</Link>} />
+                  <Divider />
+                  <CardContent>
+                    {item.description}
+                  </CardContent>
+                </Card>
               </Grid>
-            ))}
+            )) : (
+              <Grid item xs={12}>
+                <Card>
+                  <CardContent>
+                    <Typography>There is no course.</Typography>
+                  </CardContent>
+                </Card>
+              </Grid>
+            )}
           </Grid>
         </Grid>
       </Grid>
